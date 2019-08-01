9f3c05c7380512c8e0dfae98e2379114a68c30e4
Paris Cornett <pariscornett@gmail.com>
2019-07-31 21:20:57 -0500

    deploy to heroku

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendData = [
+    {
+        name: "Paris",
+        photo: "https://via.placeholder.com",
+        scores: [
+            1,
+            3,
+            5,
+            2,
+            5,
+            2,
+            4,
+            1,
+            4,
+            3
+        ]
+    }
+];
+
+module.exports = friendData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,19 @@
 //linking to friends.js where our survey data is being held as objects
 var friendData = require("../data/friends");
+var friends = ""
 
 //routes to html files 
 module.exports = function(app) {
     //routes to the API Friends List "page" and displays our friendData (stored in friends.js a JS object) as JSON
-   app.get("/api/friends", function(req, res) {
-    res.json(friendData);
-  });
+    app.get("/api/friends", function(req, res) {
+        res.json(friendData);
+    });
+
+    app.post("/api/friends", function(req,res){
+        console.log(req.body);
+        friends.push(req.body);
+        // var userInput = req.body.name;
+        // console.log(userInput);
+        // res.send(userInput);   
+    })
 }
